a6f1e347498f72b4af745f50b6e07449e353bee6
Harald Steinlechner <haraldsteinlechner@gmail.com>
2019-07-31 18:01:12 +0200

    update base

--- src/InteractiveNet471/InteractiveTest.fsx
+++ src/InteractiveNet471/InteractiveTest.fsx
@@ -4,8 +4,6 @@
 #else
 #endif
 
-open Aardvark.Base
-
 // prerequisites: use 64bit interactive (e.g. in visual studio: tools/options/f# tools/64bit interactive must be true)
 
 module InteractiveHelper = 
@@ -17,13 +15,16 @@ module InteractiveHelper =
         let assemblies =
             [
                 Assembly.Load("Aardvark.SceneGraph")
+                Assembly.Load("Aardvark.SceneGraph.IO")
             ]
         for a in assemblies do
             Introspection.RegisterAssembly a
 
     let inTemp (f : string -> 'a) =
         let last = System.Environment.CurrentDirectory
-        let current = System.IO.Path.GetTempPath()
+        let guid = System.Guid.NewGuid()
+        let current = System.IO.Path.Combine(System.IO.Path.GetTempPath(), string guid)
+        System.IO.Directory.CreateDirectory current |> ignore
         System.Environment.CurrentDirectory <- current
         let r = f current
         System.Environment.CurrentDirectory <- last
@@ -39,9 +40,7 @@ module InteractiveHelper =
             Ag.initialize()
             let gl = Assembly.Load("Aardvark.Rendering.GL")
             let vk = Assembly.Load("Aardvark.Rendering.Vulkan")
-            Aardvark.UnpackNativeDependenciesToBaseDir(gl,currentDir)
-            Aardvark.UnpackNativeDependenciesToBaseDir(vk,currentDir)
-            Aardvark.Init()
+            Aardvark.Init(currentDir)
             printfn "glvm ~> %A" (Aardvark.Base.DynamicLinker.tryLoadLibrary "glvm")
             printfn "vkvm ~> %A" (Aardvark.Base.DynamicLinker.tryLoadLibrary "vkvm")
         )
