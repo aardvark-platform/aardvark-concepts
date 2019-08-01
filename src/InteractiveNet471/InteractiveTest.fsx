828976ca80d4417d4356c374394b2f567b0d579f
Harald Steinlechner <haraldsteinlechner@gmail.com>
2019-08-01 16:06:51 +0200

    Fsi boilerplate now uses code from aardvark.rendering

--- src/InteractiveNet471/InteractiveTest.fsx
+++ src/InteractiveNet471/InteractiveTest.fsx
@@ -4,49 +4,8 @@
 #else
 #endif
 
-// prerequisites: use 64bit interactive (e.g. in visual studio: tools/options/f# tools/64bit interactive must be true)
-
-module InteractiveHelper = 
-    open System.Reflection
-    open Aardvark.Base
-    open Aardvark.Base.Incremental
-    open System.IO.Compression
-    let loadPluginsManually() = 
-        let assemblies =
-            [
-                Assembly.Load("Aardvark.SceneGraph")
-                Assembly.Load("Aardvark.SceneGraph.IO")
-            ]
-        for a in assemblies do
-            Introspection.RegisterAssembly a
-
-    let inTemp (f : string -> 'a) =
-        let last = System.Environment.CurrentDirectory
-        let guid = System.Guid.NewGuid()
-        let current = System.IO.Path.Combine(System.IO.Path.GetTempPath(), string guid)
-        System.IO.Directory.CreateDirectory current |> ignore
-        System.Environment.CurrentDirectory <- current
-        let r = f current
-        System.Environment.CurrentDirectory <- last
-        r
-
-    let init() =
-        if not System.Environment.Is64BitProcess then
-            failwith "F# interactive shell must be 64bit (e.g. in visual studio: tools/options/f# tools/64bit interactive must be true)."
-        inTemp (fun currentDir ->
-            printfn "temp for native dependencies: %s" currentDir
-            loadPluginsManually()
-            Mod.initialize()
-            Ag.initialize()
-            let gl = Assembly.Load("Aardvark.Rendering.GL")
-            let vk = Assembly.Load("Aardvark.Rendering.Vulkan")
-            Aardvark.Init(currentDir)
-            printfn "glvm ~> %A" (Aardvark.Base.DynamicLinker.tryLoadLibrary "glvm")
-            printfn "vkvm ~> %A" (Aardvark.Base.DynamicLinker.tryLoadLibrary "vkvm")
-        )
-
-
-InteractiveHelper.init()
+#load @"..\..\paket-files\aardvark-platform\aardvark.rendering\src\Application\Aardvark.Application.Utilities\FsiHelper.fsx"
+FsiHelper.InteractiveHelper.init()
 
 
 open System
